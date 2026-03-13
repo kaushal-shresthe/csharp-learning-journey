0bd06e8a6c40ab8e4d976660cfefd4f8fe134a87
kaushal-shresthe <kaushalshrestha441@gmail.com>
2026-03-13 15:08:02 +0545

    Initialized 01-csharp-basics Repo

---
+++ 01-csharp-basics/ConsoleClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_csharp_basics
+{
+    internal class ConsoleClass
+    {
+        public static void Run()
+        {
+            // Properties of Console Class are Title, ForgroundColor, BackGroundColor, CursorSize
+
+            // Methods of Console Class are Clear(), Beed(), ResetColor(), Write(), WriteLine(), Read(), ReadLine(), ReadKey()
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ResetColor();
+            Console.Title = "Console Class"; // Set Console Title
+            string Currenttitle = Console.Title; // Get Console Title
+            Console.WriteLine($"Current Title of Console is {Currenttitle}");
+            Console.Beep();
+            Console.Beep();
+            Console.CursorSize = 50;
+            Console.ReadLine();
+
+            // ReadKey() Method 
+            Console.WriteLine("Enter a Key");
+            int var1 = Console.Read();
+            Console.WriteLine($"ASCII Value of the Entered Key is: {var1}");
+            Console.WriteLine("Enter Another Key");
+            ConsoleKeyInfo var2 = Console.ReadKey();
+            Console.WriteLine($"\nEntered Key: {var2.Key} KeyChar:{var2.KeyChar} ASCII:{(int)var2.KeyChar}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01-csharp-basics/InputOutput.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_csharp_basics
+{
+    internal class InputOutput
+    {
+        public static void Run()
+        {
+            // Output Method
+            Console.Write("Hello");
+            Console.WriteLine("Sir");
+            Console.WriteLine("Welcome to C# Journey");
+
+            // Input Methods
+            Console.Write("Enter Your Name: ");
+            Console.WriteLine();
+            string name = Console.ReadLine();
+            Console.Write("Enter Your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}");
+        }
+
+        public static void StudentMarksCalc()
+        {
+            Console.WriteLine("Enter Student Details");
+            Console.WriteLine("Enter Registration Number");
+            int RegdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter Marks of three Subjects:");
+            Console.WriteLine("Subject1");
+            int Mark1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject2");
+            int Mark2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject3");
+            int Mark3 = Convert.ToInt32(Console.ReadLine());
+            int TotalMarks = Mark1 + Mark2 + Mark3;
+            int AverageMark = TotalMarks / 3;
+
+            //Display the Student Details
+            Console.WriteLine("\nStudent Details are as Follows:");
+            Console.WriteLine($"Registration Number: {RegdNumber}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Total Marks : {TotalMarks}");
+            Console.WriteLine($"Average Mark: {AverageMark}");
+        }
+    }
+}
