8ce02f79f53c5a87cf33f4da1390f7f90b97819b
kaushal-shresthe <kaushalshrestha441@gmail.com>
2026-03-13 14:38:29 +0545

    Initialized Repo

---
+++ csharp-learning-journey/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_learning_journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to CSharp Learning Journey");
+        }
+    }
+}
